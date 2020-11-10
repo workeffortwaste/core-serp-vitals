6a163d27e869845447cb6711eac67c488431cdc1
Chris Johnson <c.johnson@bamboonine.co.uk>
2020-11-10 16:33:58 +0000

    Handle missing information from CrUX.

--- src/index.js
+++ src/index.js
@@ -17,9 +17,7 @@
   const css = `
   <style>
   #serpVitals {color: #4d5156; font-size: .75rem;}
-  #serpVitals .red,
-  #serpVitals .green,
-  #serpVitals .orange{
+  #serpVitals span{
     font-weight:bold;
   }
   #serpVitals .red {color: #ff4e42}
@@ -31,11 +29,12 @@
 
   const constraints = {
     lcp: { min: 2.5, max: 4 },
-    fid: { min: 100, max: 300 },
+    fid: { min: 0.1, max: 0.3 },
     cls: { min: 0.1, max: 0.25 }
   }
 
   const getColor = (type, score) => {
+    if (score === 'N/A') { return '' }
     if (score > constraints[type].max) {
       return 'red'
     }
@@ -46,12 +45,32 @@
   }
 
   records().then(metrics => {
+    metrics.forEach(metric => {
+      if (metric !== null) {
+        if (!metric.record.metrics.largest_contentful_paint) {
+          metric.record.metrics.largest_contentful_paint = { percentiles: { p75: 'N/A' } }
+        } else {
+          metric.record.metrics.largest_contentful_paint.percentiles.p75 /= 1000
+        }
+
+        if (!metric.record.metrics.first_input_delay) {
+          metric.record.metrics.first_input_delay = { percentiles: { p75: 'N/A' } }
+        } else {
+          metric.record.metrics.first_input_delay.percentiles.p75 /= 1000
+        }
+
+        if (!metric.record.metrics.cumulative_layout_shift) {
+          metric.record.metrics.cumulative_layout_shift = { percentiles: { p75: 'N/A' } }
+        }
+      }
+    })
+
     serpArray.forEach((e, k) => {
       if (metrics[k] !== null) {
         e.insertAdjacentHTML('afterend', `
       <div id="serpVitals">
-        LCP:<span class="${getColor('lcp', metrics[k].record.metrics.largest_contentful_paint.percentiles.p75 / 1000)}">${metrics[k].record.metrics.largest_contentful_paint.percentiles.p75 / 1000}s</span>
-        FID:<span class="${getColor('fid', metrics[k].record.metrics.first_input_delay.percentiles.p75)}">${metrics[k].record.metrics.first_input_delay.percentiles.p75 / 1000}s</span>
+        LCP:<span class="${getColor('lcp', metrics[k].record.metrics.largest_contentful_paint.percentiles.p75)}">${metrics[k].record.metrics.largest_contentful_paint.percentiles.p75}</span>
+        FID:<span class="${getColor('fid', metrics[k].record.metrics.first_input_delay.percentiles.p75)}">${metrics[k].record.metrics.first_input_delay.percentiles.p75}</span>
         CLS:<span class="${getColor('cls', metrics[k].record.metrics.cumulative_layout_shift.percentiles.p75)}">${metrics[k].record.metrics.cumulative_layout_shift.percentiles.p75}</span>
       </div>
   `)
